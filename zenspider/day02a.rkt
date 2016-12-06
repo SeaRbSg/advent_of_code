db0e2ca47ff46520e529a595572ee2adffea70f1
Ryan Davis <ryand-ruby@zenspider.com>
2016-12-06 00:37:29 -0800

    rearranged order of for/fold to match something I want to do

--- zenspider/day02a.rkt
+++ zenspider/day02a.rkt
@@ -1,9 +1,9 @@
 #lang racket
 
 (define (decode lines)
-  (define-values (_ acc)
-    (for/fold ([pos 5]
-               [acc 0])
+  (define-values (acc _)
+    (for/fold ([acc 0]
+               [pos 5])
               ([line (string-split lines)])
       (define n (for/fold ([pos pos])
                           ([dir (in-string line)])
@@ -12,7 +12,7 @@
                     [#\D (if (> (+ pos 3) 9)      pos (+ pos 3))]
                     [#\L (if (= 1 (modulo pos 3)) pos (- pos 1))]
                     [#\R (if (= 0 (modulo pos 3)) pos (+ pos 1))])))
-      (values n (+ (* 10 acc) n))))
+      (values (+ (* 10 acc) n) n)))
   acc)
 
 (decode (call-with-input-file "day02a.txt" port->string))
