eac8f91614b9abb6484407fe781ab08051f1fda4
Ryan Davis <ryand-ruby@zenspider.com>
2016-12-06 00:39:08 -0800

    Is there a way to tell magit diff to ignore whitespace changes?
    I don't like separating these commits just for diffs. I guess you also
    need to do the same with github and the like. :(

--- zenspider/day02a.rkt
+++ zenspider/day02a.rkt
@@ -12,14 +12,14 @@
   (for/fold/1 ([acc 0]
                [pos 5])
               ([line (string-split lines)])
-      (define n (for/fold ([pos pos])
-                          ([dir (in-string line)])
-                  (match dir
-                    [#\U (if (< (- pos 3) 1)      pos (- pos 3))]
-                    [#\D (if (> (+ pos 3) 9)      pos (+ pos 3))]
-                    [#\L (if (= 1 (modulo pos 3)) pos (- pos 1))]
-                    [#\R (if (= 0 (modulo pos 3)) pos (+ pos 1))])))
-      (values (+ (* 10 acc) n) n)))
+    (define n (for/fold ([pos pos])
+                        ([dir (in-string line)])
+                (match dir
+                  [#\U (if (< (- pos 3) 1)      pos (- pos 3))]
+                  [#\D (if (> (+ pos 3) 9)      pos (+ pos 3))]
+                  [#\L (if (= 1 (modulo pos 3)) pos (- pos 1))]
+                  [#\R (if (= 0 (modulo pos 3)) pos (+ pos 1))])))
+    (values (+ (* 10 acc) n) n)))
 
 (decode (call-with-input-file "day02a.txt" port->string))
 
