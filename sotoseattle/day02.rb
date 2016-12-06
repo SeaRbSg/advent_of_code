fb406bbb705cd8e98983b4bdc277dc3e629c58bd
Javier Soto <sotoseattle@gmail.com>
2016-12-06 07:47:06 -0800

    add day2a

---
+++ sotoseattle/day02.rb
@@ -0,0 +1,26 @@
+module Adventus
+
+  DICT = [[1,2,3],[4,5,6],[7,8,9]]
+  WALK = {"U" => [-1,0], "D" => [+1,0], "L" => [0,-1], "R" => [0,+1]}
+
+  def bathroom_code instructions
+    spot = [1,1]
+
+    instructions.split("\n").map(&:chars).map do |set|
+      set.each {|step| spot = add(spot, WALK[step]) }
+      DICT[spot.first][spot.last]
+    end.join
+  end
+
+  def add x, y
+    a, b = [x.first + y.first, x.last + y.last]
+    [crop(a), crop(b)]
+  end
+
+  def crop n
+    return 0 if n < 0
+    return 2 if n > 2
+    n
+  end
+
+end
