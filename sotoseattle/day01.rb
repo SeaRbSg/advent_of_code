acf53a77bcfda8c572d3ea50827bea08401d2fe6
Javier Soto <sotoseattle@gmail.com>
2016-12-06 07:57:55 -0800

    minor refactoring and house cleaning

--- sotoseattle/day01.rb
+++ sotoseattle/day01.rb
@@ -1,7 +1,7 @@
 module Adventus
 
-  WHEEL = %w{N E S W N}
-  COORD = {"N" => [1, 0], "S" => [-1, 0], "W" => [0, -1], "E" => [0, 1]}
+  WHEEL_D1 = %w{N E S W N}
+  COORD_D1 = {"N" => [1, 0], "S" => [-1, 0], "W" => [0, -1], "E" => [0, 1]}
 
   def parse_directions dirs
     dirs.split(", ").map {|e| e.scan(/(R|L)(\d+)/).flatten}
@@ -16,7 +16,7 @@ module Adventus
   end
 
   def turn original, dir
-    arr = (dir == "R" ? WHEEL : WHEEL.reverse)
+    arr = (dir == "R" ? WHEEL_D1 : WHEEL_D1.reverse)
     arr[arr.find_index(original).next]
   end
 
@@ -25,7 +25,7 @@ module Adventus
 
     parse_directions(input).each do |dir, n|
       tack = turn tack, dir
-      jump = COORD[tack].map { |e| e * n.to_i }
+      jump = COORD_D1[tack].map { |e| e * n.to_i }
       spot = spot.zip(jump).map{|e| e.reduce(:+)}
     end
 
@@ -38,13 +38,14 @@ module Adventus
     parse_directions(input).each do |dir, n|
       tack = turn tack, dir
       n.to_i.times do
-        a, b, c, d = spot + COORD[tack]
+        a, b, c, d = spot + COORD_D1[tack]
         spot = [a + c, b + d]
 
         return distance(spot) if path.include?(spot)
         path << spot
       end
     end
+
     distance spot
   end
 
--- sotoseattle/day02.rb
+++ sotoseattle/day02.rb
@@ -1,14 +1,16 @@
 module Adventus
 
-  DICT = [[1,2,3],[4,5,6],[7,8,9]]
-  WALK = {"U" => [-1,0], "D" => [+1,0], "L" => [0,-1], "R" => [0,+1]}
+  DICT_D2 = [[1, 2, 3],
+             [4, 5, 6],
+             [7, 8, 9]]
+  WALK_D2 = {"U" => [-1,0], "D" => [+1,0], "L" => [0,-1], "R" => [0,+1]}
 
   def bathroom_code instructions
     spot = [1,1]
 
     instructions.split("\n").map(&:chars).map do |set|
-      set.each {|step| spot = add(spot, WALK[step]) }
-      DICT[spot.first][spot.last]
+      set.each {|step| spot = add(spot, WALK_D2[step]) }
+      DICT_D2[spot.first][spot.last]
     end.join
   end
 
