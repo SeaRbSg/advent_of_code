e1511debdae8a8f8875b8e9ffce6c7887cc493d0
Ryan Davis <ryand-ruby@zenspider.com>
2016-12-16 02:52:47 -0800

    day 5a.
    I spent a LOT of time trying to make this faster. WAY more time than
    I'll ever get back. But, things I've learned: racket parallelization
    pretty much sucks. Persistent caching in racket is hard because
    serialization costs a lot... but damn is the code clean.
    
    This solution is more difficult than just using md5 straight, but it
    IS slightly faster.

---
+++ zenspider/day05a.rkt
@@ -0,0 +1,41 @@
+#lang racket
+
+(require openssl/md5)
+(require openssl/sha1)
+
+(define (md5hash s n)
+  (define x (md5-bytes (open-input-string (string-append s (number->string n)))))
+  (and (zero? (bytes-ref x 0))
+       (zero? (bytes-ref x 1))
+       (zero? (bitwise-and #xF0 (bytes-ref x 2)))
+       (bytes->hex-string x)))
+
+(define (unlock key len [start 0])
+  (string-append*
+   (reverse
+    (for/fold ([l '()])
+              ([i (in-naturals start)]
+               #:break (>= (length l) len)
+               #:when (md5hash key i))
+      (cons (substring (md5hash key i) 5 6) l)))))
+
+(module+ test
+  (require rackunit)
+
+  (check-equal? (md5hash "abc" 3231928) #f)
+  (check-equal? (md5hash "abc" 3231929) "00000155f8105dff7f56ee10fa9b9abd")
+
+  (check-equal? (unlock "abc" 0) "")
+  (check-equal? (unlock "abc" 1 3231928) "1")
+  (check-equal? (unlock "abc" 1 3000000) "1")
+  (check-equal? (unlock "abc" 1) "1")
+  (check-equal? (unlock "abc" 2) "18")
+  (check-equal? (unlock "abc" 3) "18f")
+  (check-equal? (unlock "abc" 8) "18f47a30")
+
+  (check-equal? (unlock "wtnhxymk" 8) "2414bc77")
+
+  (displayln "done"))
+
+(module+ main
+  (time (displayln (unlock "wtnhxymk" 8))))
