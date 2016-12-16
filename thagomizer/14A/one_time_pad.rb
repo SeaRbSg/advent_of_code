7e6e13c93bd7cff14b28debe4882a2d1551a973a
Aja Hammerly <aja.hammerly@gmail.com>
2016-12-16 14:10:33 -0800

    Cleaner version of day 14, star 1

--- thagomizer/14A/one_time_pad.rb
+++ thagomizer/14A/one_time_pad.rb
@@ -1,29 +1,45 @@
 require 'digest'
 
-data = File.read(ARGV[0]).strip
+class PasswordGenerator
+  attr_accessor :password_indices
 
-passwords = []
-$hashes = {}
-
-i = 0
-
-while passwords.length < 64 do
-  $hashes[i] ||= Digest::MD5.hexdigest "#{data}#{i}"
-
-  if $hashes[i] =~ /(\w)\1\1/ then
-    to_find = $1 * 5
-
-    (1..1_000).each do |j|
-      $hashes[i + j] ||= Digest::MD5.hexdigest "#{data}#{i + j}"
-
-      if $hashes[i + j].include? to_find then
-        passwords << i
-        break
-      end
-    end
+  def initialize salt
+    @salt             =    salt
+    @hashes           = {}
+    @password_indices = []
   end
 
-  i += 1   # IMPORTANT!
+  def generate_n_passwords n
+    n -= 1   # Fencepost
+    return if password_indices[n]
+
+    i = password_indices.last || 0
+
+    until password_indices[n] do
+      @hashes[i] ||= Digest::MD5.hexdigest "#{@salt}#{i}"
+
+      if @hashes[i] =~ /(\w)\1\1/ then
+        to_find = $1 * 5
+
+        (1..1_000).each do |j|
+          @hashes[i + j] ||= Digest::MD5.hexdigest "#{@salt}#{i + j}"
+
+          if @hashes[i + j].include? to_find then
+            @password_indices << i
+            break
+          end
+        end
+      end
+
+      i += 1   # IMPORTANT!
+    end
+  end
 end
 
-puts passwords.last
+salt = File.read(ARGV[0]).strip
+
+pg = PasswordGenerator.new salt
+
+pg.generate_n_passwords 64
+
+puts pg.password_indices.last
