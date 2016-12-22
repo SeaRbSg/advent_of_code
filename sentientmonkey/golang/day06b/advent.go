86b9aa873764d0023448925c86fc37adf6f9c2b4
Scott Windsor <swindsor@gmail.com>
2016-12-21 23:52:00 -0800

    Day 6b (go)

---
+++ sentientmonkey/golang/day06b/advent.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+type Frequency struct {
+	Counts []map[rune]int
+}
+
+func (frequency *Frequency) AddMessage(message string) {
+	for i, r := range message {
+		if len(frequency.Counts) <= i {
+			frequency.Counts = append(frequency.Counts, make(map[rune]int))
+		}
+		frequency.Counts[i][r] += 1
+	}
+}
+
+func (frequency *Frequency) AddSignal(signal string) {
+	for _, message := range strings.Split(signal, "\n") {
+		frequency.AddMessage(message)
+	}
+}
+
+func (frequency *Frequency) ErrorCorrected() string {
+	message := ""
+	for _, row := range frequency.Counts {
+		var minimum int
+		var topLetter rune
+		for letter, count := range row {
+			if minimum == 0 || count <= minimum {
+				topLetter = letter
+				minimum = count
+			}
+		}
+		message = message + string(topLetter)
+	}
+
+	return message
+}
+
+func main() {
+	frequency := &Frequency{}
+	reader := bufio.NewReader(os.Stdin)
+	scanner := bufio.NewScanner(reader)
+	for scanner.Scan() {
+		frequency.AddSignal(scanner.Text())
+	}
+	fmt.Println(frequency.ErrorCorrected())
+}
