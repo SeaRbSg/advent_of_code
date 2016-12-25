8ea2cec56f405dbf2f64c080d872608c2996e798
Ryan Davis <ryand-ruby@zenspider.com>
2016-12-24 17:09:27 -0800

    cleanup of 12

--- zenspider/12a.rkt
+++ zenspider/12a.rkt
@@ -3,26 +3,24 @@
 (require "myutils.rkt")
 
 (struct register (pc a b c d) #:transparent)
+(define lookup (hasheq 'pc register-pc  ; UGH
+                       'a  register-a
+                       'b  register-b
+                       'c  register-c
+                       'd  register-d))
+(define (get reg k)                     ; UGH
+  ((hash-ref lookup k) reg))
 
-(define computer (register 0 0 0 0 0))
-
-(define lookup (hasheq 'pc register-pc
-                       'a register-a
-                       'b register-b
-                       'c register-c
-                       'd register-d))
-
-(define (up reg k v)                    ; UGH! struct-copy doesn't work w/ vars
+(define (set reg k v)                   ; UGH! struct-copy doesn't work w/ vars
   (define pc (register-pc reg))
-
   (match k
-    ['pc (struct-copy register reg [pc (+ v pc)])]
-    ['a  (struct-copy register reg [a v] [pc (add1 pc)])]
-    ['b  (struct-copy register reg [b v] [pc (add1 pc)])]
-    ['c  (struct-copy register reg [c v] [pc (add1 pc)])]
-    ['d  (struct-copy register reg [d v] [pc (add1 pc)])]))
+    ['pc (struct-copy register reg       [pc (+ v pc)])]
+    ['a  (struct-copy register reg [a v] [pc (+ 1 pc)])]
+    ['b  (struct-copy register reg [b v] [pc (+ 1 pc)])]
+    ['c  (struct-copy register reg [c v] [pc (+ 1 pc)])]
+    ['d  (struct-copy register reg [d v] [pc (+ 1 pc)])]))
 
-(define (run in)
+(define (run in [initial-values #f])
   (define (compile in)
     (define (n/s x) (or (string->number x) (string->symbol x)))
     (for/vector ([inst (parse-lines-of-words in)])
@@ -33,27 +31,26 @@
         [(list "jnz" (app n/s x) (app n/s y)) `(jnz ,x ,y)])))
 
   (define ops (compile in))
-  (define ops-max (vector-length ops))
+  (define max (vector-length ops))
   (define pc 0)
 
   (define (execute reg)
-    (define (n/v x) (if (number? x) x ((hash-ref lookup x) reg)))
-    (define (cpy x y) (up reg y (n/v x)))
-    (define (jnz x y) (up reg 'pc (if (zero? (n/v x)) 1 y)))
-    (define (inc x)   (up reg x (add1 (n/v x))))
-    (define (dec x)   (up reg x (sub1 (n/v x))))
+    (define (n/v x)   (if (number? x) x (get reg x)))
+    (define (cpy x y) (set reg y (n/v x)))
+    (define (jnz x y) (set reg 'pc (if (zero? (n/v x)) 1 y)))
+    (define (inc x)   (set reg x (add1 (n/v x))))
+    (define (dec x)   (set reg x (sub1 (n/v x))))
 
-    (define pc (register-pc reg))
-    (define op (and (< pc ops-max) (vector-ref ops pc)))
+    (define pc (get reg 'pc))
 
-    (match op
+    (match (and (< pc max) (vector-ref ops pc))
       [(list 'cpy x y) (execute (cpy x y))]
       [(list 'inc x)   (execute (inc x))]
       [(list 'dec x)   (execute (dec x))]
       [(list 'jnz x y) (execute (jnz x y))]
       [_ reg]))
 
-  (execute (register 0 0 0 0 0)))
+  (execute (apply register (or initial-values '(0 0 0 0 0)))))
 
 (module+ test
   (require rackunit)
@@ -65,5 +62,4 @@
 
   (check-equal? (run (data-file 12))
                 (register 23 318117 196418 0 0))
-
   )
