346346d2f8556eea047a54ee3dda77b19d7feff6
Ryan Davis <ryand-ruby@zenspider.com>
2017-12-16 15:08:06 -0800

    01a

---
+++ zenspider/2017/01.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby -w
+
+class Problem01a
+  def run input
+    a = input.chars.to_a
+    b = a.dup.cycle(2).drop 1
+    a.zip(b).inject(0) { |acc, (n, m)| acc + (n == m ? n.to_i : 0) }
+  end
+end
+
+if __FILE__ == $0 then
+  if ARGV.empty? then
+    require "minitest/autorun"
+
+    class Test01 < Minitest::Test
+      def test_a
+        assert_equal 3, Problem01a.new.run("1122")
+        assert_equal 4, Problem01a.new.run("1111")
+        assert_equal 0, Problem01a.new.run("1234")
+        assert_equal 9, Problem01a.new.run("91212129")
+      end
+    end
+  else
+    input = ARGF.read.chomp
+    p Problem01a.new.run input
+  end
+end
