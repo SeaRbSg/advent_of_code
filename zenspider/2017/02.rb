273f468c3c18518b5c038d09ecacf0f465284d44
Ryan Davis <ryand-ruby@zenspider.com>
2017-12-16 16:34:38 -0800

    Refactored

--- zenspider/2017/02.rb
+++ zenspider/2017/02.rb
@@ -1,14 +1,6 @@
 #!/usr/bin/env ruby -w
 
-def lines_of_numbers input
-  input.lines.map(&:integers)
-end
-
-class String
-  def integers
-    self.scan(/\d+/).map(&:to_i)
-  end
-end
+require "./utils.rb"
 
 class Array
   def minmax_diff
---
+++ zenspider/2017/utils.rb
@@ -0,0 +1,9 @@
+def lines_of_numbers input
+  input.lines.map(&:integers)
+end
+
+class String
+  def integers
+    self.scan(/\d+/).map(&:to_i)
+  end
+end
