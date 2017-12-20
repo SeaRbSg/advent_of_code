646049e51a27f86ab0d8146da080fa1d14155140
Ryan Davis <ryand-ruby@zenspider.com>
2017-12-19 18:38:25 -0800

    04a

---
+++ zenspider/2017/04.rb
@@ -0,0 +1,45 @@
+#!/usr/bin/env ruby -w
+
+class Problem04a
+  def run path
+    File.readlines(path).count { |line| valid? line }
+  end
+
+  def valid? input
+    words = input.split(/\s+/)
+    words.count == words.uniq.count
+  end
+end
+
+class Problem04b < Problem04a
+end
+
+if __FILE__ == $0 then
+  if ARGV.empty? then
+    require "minitest/autorun"
+
+    class Test04 < Minitest::Test
+      def assert_valid input
+        assert_operator Problem04a.new, :valid?, input
+      end
+
+      def refute_valid input
+        refute_operator Problem04a.new, :valid?, input
+      end
+
+      def test_a_valid
+        assert_valid "aa bb cc dd ee"
+        assert_valid "aa bb cc dd aaa"
+        refute_valid "aa bb cc dd aa"
+      end
+
+      def test_b
+        skip
+      end
+    end
+  else
+    path = ARGV.shift
+    p Problem04a.new.run path
+    p Problem04b.new.run path
+  end
+end
