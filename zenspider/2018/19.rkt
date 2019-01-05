5fa0fd55dcb9cc0932fb5d0677b03ba13c996cf8
Ryan Davis <ryand-ruby@zenspider.com>
2019-01-05 14:42:22 -0800

    Optimize 19a for racket.
    Goes from ~0.72s to 0.53s.
    
    This was a pretty hefty overhaul without changing the overall
    architecture (in rough order):
    
    + switch setters to static compilation = ~2-4 ms    (~0.75s -> ~0.72s)
    + mutable structs                      = ~150-200ms (~0.70s -> ~0.55s)
    + reg struct -> vector                 = ~0ms (possibly -5ms)
    + inst struct -> vector                = ~0ms (~0.59s)
    + currying reg-[gs]etter               = ~150ms ? (~0.45s)
    
    Overall, it would have probably been fine to switch to mutable structs
    and stop there, but what I really wanted was to have the function
    stored in the instruction do ALL the work:
    
    + Access slots (or immediate values), via static access
    + Compute the result, as needed
    + Store the result, via static access
    
    It turns out that this didn't make THAT much of a difference, but that
    might be because of the length of the current problem. Maybe I could
    figure out the input enough to make it run longer and test that theory.

--- zenspider/2018/19.rkt
+++ zenspider/2018/19.rkt
@@ -1,131 +1,104 @@
-#lang racket
+#lang racket/base
 
-(require "myutils.rkt")
+(require racket/match
+         racket/string
+         racket/list
+         racket/function
+         "myutils.rkt")
 
-(struct reg  (pc r0 r1 r2 r3 r4 r5) #:transparent)
-(struct inst (op a b c) #:transparent)
-(struct slot (n) #:transparent)
+;; switch setters to static compilation = ~2-4 ms    (~0.75s -> ~0.72s)
+;; mutable structs                      = ~150-200ms (~0.70s -> ~0.55s)
+;; reg struct -> vector                 = ~0ms (possibly -5ms)
+;; inst struct -> vector                = ~0ms (~0.59s)
+;; currying reg-[gs]etter               = ~150ms ? (~0.45s)
 
-(define (new/r) (reg 0 0 0 0 0 0 0))
+(define (inst op a b) (vector op a b))
+(define (inst-op i) (vector-ref i 0))
+(define (inst-a  i) (vector-ref i 1))
+(define (inst-b  i) (vector-ref i 2))
 
-(define (reg-set-pc r v) (struct-copy reg r [pc v]))
-(define (reg-set-r0 r v) (struct-copy reg r [r0 v]))
-(define (reg-set-r1 r v) (struct-copy reg r [r1 v]))
-(define (reg-set-r2 r v) (struct-copy reg r [r2 v]))
-(define (reg-set-r3 r v) (struct-copy reg r [r3 v]))
-(define (reg-set-r4 r v) (struct-copy reg r [r4 v]))
-(define (reg-set-r5 r v) (struct-copy reg r [r5 v]))
+(define (new/r) (vector 0 0 0 0 0 0 0))
 
-(define (reg-set r n v)
-  (case n
-    [(0) (reg-set-r0 r v)]
-    [(1) (reg-set-r1 r v)]
-    [(2) (reg-set-r2 r v)]
-    [(3) (reg-set-r3 r v)]
-    [(4) (reg-set-r4 r v)]
-    [(5) (reg-set-r5 r v)]
-    [else (raise-arguments-error 'reg-set "bad value" "r" r "n" n "v" v)]))
+(define ((reg-getter n) r) (vector-ref r n))
+(define reg-r0 (reg-getter 0))          ; for tests
+(define reg-pc (reg-getter 6))          ; for exec/go
 
-(define (reg-get-r0 r) (reg-r0 r))
-(define (reg-get-r1 r) (reg-r1 r))
-(define (reg-get-r2 r) (reg-r2 r))
-(define (reg-get-r3 r) (reg-r3 r))
-(define (reg-get-r4 r) (reg-r4 r))
-(define (reg-get-r5 r) (reg-r5 r))
+(define (reg-set-pc r v) (vector-set! r 6 v) r)
 
-(define (reg-get r n)
-  (case n
-    [(0) (reg-get-r0 r)]
-    [(1) (reg-get-r1 r)]
-    [(2) (reg-get-r2 r)]
-    [(3) (reg-get-r3 r)]
-    [(4) (reg-get-r4 r)]
-    [(5) (reg-get-r5 r)]
-    [else (raise-arguments-error 'reg-get "bad value" "r" r "n" n)]))
-
-(define ((op/rr fn) r in)
-  (reg-set r (inst-c in) (fn (reg-get r (inst-a in)) (reg-get r (inst-b in)))))
-
-(define ((op/ri fn) r in)
-  (reg-set r (inst-c in) (fn (reg-get r (inst-a in)) (inst-b in))))
-
-(define ((op/ir fn) r in)
-  (reg-set r (inst-c in) (fn            (inst-a in)  (reg-get r (inst-b in)))))
-
-(define ((op/r) r in)
-  (reg-set r (inst-c in) (reg-get r (inst-a in))))
-
-(define ((op/i) r in)
-  (reg-set r (inst-c in) (inst-a in)))
+(define ((reg-setter n) r v) (vector-set! r n v) r)
 
 (define (b->i b) (if b 1 0))
-
 (define gti (compose b->i >))
 (define eqi (compose b->i =))
 
-(define addr (op/rr +))
-(define addi (op/ri +))
-(define mulr (op/rr *))
-(define muli (op/ri *))
-(define banr (op/rr bitwise-and))
-(define bani (op/ri bitwise-and))
-(define borr (op/rr bitwise-ior))
-(define bori (op/ri bitwise-ior))
-(define setr (op/r))
-(define seti (op/i))
-(define gtir (op/ir gti))
-(define gtri (op/ri gti))
-(define gtrr (op/rr gti))
-(define eqir (op/ir eqi))
-(define eqri (op/ri eqi))
-(define eqrr (op/rr eqi))
-
 (define (parse s/f)
-  (define out
-   (for/list ([line (parse-lines-of-atoms s/f)])
-     (match line
-       [`(|#ip| ,n)      (slot n)]
-       [`(addr ,a ,b ,c) (inst addr a b c)]
-       [`(addi ,a ,b ,c) (inst addi a b c)]
-       [`(mulr ,a ,b ,c) (inst mulr a b c)]
-       [`(muli ,a ,b ,c) (inst muli a b c)]
-       [`(banr ,a ,b ,c) (inst banr a b c)]
-       [`(bani ,a ,b ,c) (inst bani a b c)]
-       [`(borr ,a ,b ,c) (inst borr a b c)]
-       [`(bori ,a ,b ,c) (inst bori a b c)]
-       [`(setr ,a ,b ,c) (inst setr a b c)]
-       [`(seti ,a ,b ,c) (inst seti a b c)]
-       [`(gtir ,a ,b ,c) (inst gtir a b c)]
-       [`(gtri ,a ,b ,c) (inst gtri a b c)]
-       [`(gtrr ,a ,b ,c) (inst gtrr a b c)]
-       [`(eqir ,a ,b ,c) (inst eqir a b c)]
-       [`(eqri ,a ,b ,c) (inst eqri a b c)]
-       [`(eqrr ,a ,b ,c) (inst eqrr a b c)]
-       [_ (list 'fucked line)])))
-  (values (car out) (list->vector (cdr out))))
+  ;; fn a b c #t #t ->
+  ;; (let ([known slot getters] ...)
+  ;;   (λ (r in) (setter r (fn (get-a) (get-b)))))
+
+  (define (wrap/inst fn a b c a/reg b/reg)
+    (define setter (reg-setter c))
+    (define wrapped-fn
+      (match (cons a/reg b/reg)
+        ['(#t . #t) (let ([ga (reg-getter a)]
+                          [gb (reg-getter b)])
+                      (λ (r in) (setter r (fn (ga r) (gb r)))))]
+        ['(#t . #f) (let ([ga (reg-getter a)])
+                      (λ (r in) (setter r (fn (ga r) b))))]
+        ['(#f . #t) (let ([gb (reg-getter b)])
+                      (λ (r in) (setter r (fn a      (gb r)))))]
+        ['(#t . no) (let ([ga (reg-getter a)])
+                      (λ (r in) (setter r     (ga r))))]
+        ['(#f . no)   (λ (r in) (setter r     a))]))
+    (inst wrapped-fn a b))
+
+  (define-values (slot rest) (split-at (parse-lines-of-atoms s/f) 1))
+
+  (define insts
+    (for/vector ([line (in-list rest)])
+      (match line
+        [`(addr ,a ,b ,c) (wrap/inst + a b c #t #t)]
+        [`(addi ,a ,b ,c) (wrap/inst + a b c #t #f)]
+        [`(mulr ,a ,b ,c) (wrap/inst * a b c #t #t)]
+        [`(muli ,a ,b ,c) (wrap/inst * a b c #t #f)]
+        [`(banr ,a ,b ,c) (wrap/inst bitwise-and a b c #t #t)]
+        [`(bani ,a ,b ,c) (wrap/inst bitwise-and a b c #t #f)]
+        [`(borr ,a ,b ,c) (wrap/inst bitwise-ior a b c #t #t)]
+        [`(bori ,a ,b ,c) (wrap/inst bitwise-ior a b c #t #f)]
+        [`(setr ,a ,b ,c) (wrap/inst 'meh a b c #t 'no)]
+        [`(seti ,a ,b ,c) (wrap/inst 'meh a b c #f 'no)]
+        [`(gtir ,a ,b ,c) (wrap/inst gti a b c #f #t)]
+        [`(gtri ,a ,b ,c) (wrap/inst gti a b c #t #f)]
+        [`(gtrr ,a ,b ,c) (wrap/inst gti a b c #t #t)]
+        [`(eqir ,a ,b ,c) (wrap/inst eqi a b c #f #t)]
+        [`(eqri ,a ,b ,c) (wrap/inst eqi a b c #t #f)]
+        [`(eqrr ,a ,b ,c) (wrap/inst eqi a b c #t #t)]
+        [_ (list 'fucked line)])))
+  (values (cadar slot) insts))
 
 (define (exec reg slot insts)
-  (define maxI (vector-length insts))
-  (define (updatePC r) (reg-set-pc r (add1 (reg-get r slot))))
+  (define maxI         (vector-length insts))
+  (define get-slot     (reg-getter slot))
+  (define (updatePC r) (reg-set-pc r (add1 (get-slot r))))
+  (define updateSlot   (reg-setter slot))
   (define (go r)
     (define pc (reg-pc r))
-    (if (>= pc maxI)
-        r
-        (begin
-          (let* ([i (vector-ref insts pc)]
-                 [r1 (reg-set r slot pc)]
-                 [r2 ((inst-op i) r1 i)]
-                 [r3 (updatePC r2)])
-            (go r3)))))
+    (if (< pc maxI)
+        (let* ([i  (vector-ref insts pc)] ; get instruction via PC
+               [r1 (updateSlot r pc)]     ; copy PC to <slot>
+               [r2 ((inst-op i) r1 i)]    ; execute instruction
+               [r3 (updatePC r2)])        ; copy <slot> + 1 to PC
+          (go r3))
+        r))
   (go reg))
 
 (define (problem1 s)
   (let-values ([(slot insts) (parse s)])
-    (exec (new/r) (slot-n slot) insts)))
+    (exec (new/r) slot insts)))
 
 (define (problem2 s)
   (let-values ([(slot insts) (parse s)])
-    (exec (reg-set (new/r) 0 1) (slot-n slot) insts)))
+    (exec ((reg-setter 0) (new/r) 1) slot insts)))
 
 (define test/file (string-join
                    '("#ip 0"
@@ -145,46 +118,32 @@
                      #f
                      (car args)))
                test/file))
+  (time (problem1 arg))
+  #;(time (problem2 arg))
+  )
 
-  (problem1 arg))
+(module+ mainx
+
+  (require profile)
+  (profile-thunk (thunk (problem1 "19.txt"))
+                 #:order 'total
+                 #:use-errortrace? #t))
 
 (module+ test
   (require rackunit)
 
-  (define r  (reg 0 3 2 1 0 0 0))
-  (define i1 (inst 42 0 1 2))
-  (define i3 (inst 42 0 3 2))
+  (define r  (vector 3 2 1 0 0 0 0))
+  (define i1 (inst 42 0 1))
+  (define i3 (inst 42 0 3))
 
-  (check-equal? (reg-get r 0) 3)
-  (check-equal? (reg-set r 1 9) (reg 0 3 9 1 0 0 0))
+  (check-equal? ((reg-getter 0) r) 3)
+  (check-equal? ((reg-setter 1) r 9) (vector 3 9 1 0 0 0 0))
 
-  (check-equal? (addr r (inst 42 0 1 2)) (reg 0 3 2 5 0 0 0))
-  (check-equal? (addi r (inst 42 0 3 2)) (reg 0 3 2 6 0 0 0))
-  (check-equal? (mulr r (inst 42 0 1 2)) (reg 0 3 2 6 0 0 0))
-  (check-equal? (muli r (inst 42 0 3 2)) (reg 0 3 2 9 0 0 0))
-  (check-equal? (banr r (inst 42 0 1 2)) (reg 0 3 2 2 0 0 0)) ; 10 & 11 == 10
-  (check-equal? (bani r (inst 42 0 3 2)) (reg 0 3 2 3 0 0 0)) ; 11 & 11 == 11
-  (check-equal? (borr r (inst 42 0 1 2)) (reg 0 3 2 3 0 0 0)) ; 11 | 10 == 11
-  (check-equal? (bori r (inst 42 0 3 2)) (reg 0 3 2 3 0 0 0)) ; 11 | 11 == 11
+  (check-equal? (reg-r0 (problem1 test/file)) 6)
 
-  (check-equal? (gtir r (inst 42 1 1 2)) (reg 0 3 2 0 0 0 0))
-  (check-equal? (gtir r (inst 42 3 1 2)) (reg 0 3 2 1 0 0 0))
-  (check-equal? (gtri r (inst 42 0 1 2)) (reg 0 3 2 1 0 0 0))
-  (check-equal? (gtri r (inst 42 0 4 2)) (reg 0 3 2 0 0 0 0))
-  (check-equal? (gtrr r (inst 42 0 1 2)) (reg 0 3 2 1 0 0 0))
-  (check-equal? (gtrr r (inst 42 1 0 2)) (reg 0 3 2 0 0 0 0))
-
-  (check-equal? (eqir r (inst 42 0 1 2)) (reg 0 3 2 0 0 0 0))
-  (check-equal? (eqir r (inst 42 2 1 2)) (reg 0 3 2 1 0 0 0))
-  (check-equal? (eqri r (inst 42 0 1 2)) (reg 0 3 2 0 0 0 0))
-  (check-equal? (eqri r (inst 42 0 3 2)) (reg 0 3 2 1 0 0 0))
-  (check-equal? (eqrr r (inst 42 0 1 2)) (reg 0 3 2 0 0 0 0))
-  (check-equal? (eqrr r (inst 42 1 1 2)) (reg 0 3 2 1 0 0 0))
-
-  (check-equal? (setr r (inst 42 0 1 2)) (reg 0 3 2 3 0 0 0))
-  (check-equal? (seti r (inst 42 6 1 2)) (reg 0 3 2 6 0 0 0))
-
-  (check-equal? (reg-r0 (problem1 test/file)) 6))
+  #;(check-equal? (reg-r0 (problem1 "19.txt")) 1248)
+  42
+  )
 
 (module+ test
   (displayln 'done))
