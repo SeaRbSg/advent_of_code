6aa88235889714a7a6c0e565e9bb8ccc4c5f7b7a
Ryan Davis <ryand-ruby@zenspider.com>
2019-12-01 20:09:21 -0800

    Finished 01a for ruby.
    14 minutes with some infra crap.

--- zenspider/2019/01.rb
+++ zenspider/2019/01.rb
@@ -1,44 +1,14 @@
 #!/usr/bin/env ruby -w
 
-# ## --- Day 1: The Tyranny of the Rocket Equation ---
-#
-# Santa has become stranded at the edge of the Solar System while delivering
-# presents to other planets! To accurately calculate his position in space,
-# safely align his warp drive, and return to Earth in time to save Christmas,
-# he needs you to bring him _measurements_ ((If only you had time to grab an
-# astrolabe.)) from _fifty stars_.
-#
-# Collect stars by solving puzzles.  Two puzzles will be made available on
-# each day in the Advent calendar; the second puzzle is unlocked when you
-# complete the first.  Each puzzle grants _one star_. Good luck!
-#
-# The Elves quickly load you into a spacecraft and prepare to launch.
-#
-# At the first Go / No Go poll, every Elf is Go until the Fuel Counter-Upper.
-# They haven't determined the amount of fuel required yet.
-#
-# Fuel required to launch a given _module_ is based on its _mass_.
-# Specifically, to find the fuel required for a module, take its mass, divide
-# by three, round down, and subtract 2.
-#
-# For example:
-#
-# * For a mass of `12`, divide by 3 and round down to get `4`, then subtract 2
-#   to get `2`.
-# * For a mass of `14`, dividing by 3 and rounding down still yields `4`, so
-#   the fuel required is also `2`.
-# * For a mass of `1969`, the fuel required is `654`.
-# * For a mass of `100756`, the fuel required is `33583`.
-#
-# The Fuel Counter-Upper needs to know the total fuel requirement.  To find
-# it, individually calculate the fuel needed for the mass of each module
-# (your puzzle input), then add together all the fuel values.
-#
-# _What is the sum of the fuel requirements_ for all of the modules on your
-# spacecraft?
+require "../2017/utils.rb"
 
 class Problem01a
+  def fuel mass
+    (mass / 3).floor - 2
+  end
+
   def run input
+    input.integers.sum { |n| fuel n }
   end
 end
 
@@ -49,8 +19,22 @@ if ARGV.empty? then
   require "minitest/autorun"
 
   class Test01 < Minitest::Test
+    def test_fuel
+      x = Problem01a.new
+
+      assert_equal 2,     x.fuel(12)
+      assert_equal 2,     x.fuel(14)
+      assert_equal 654,   x.fuel(1969)
+      assert_equal 33583, x.fuel(100756)
+    end
+
     def test_a
-      flunk
+      input = "12\n14\n1969\n100756\n"
+
+      sum = Problem01a.new.run input
+      exp = 2 + 2 + 654 + 33583
+
+      assert_equal exp, sum
     end
 
     def test_b
